4fc19b5aea4cbf00a8cd8663a14034f356d882d3
yaswanthnaik <92287838+yaswanthnaik@users.noreply.github.com>
2021-11-02 10:47:41 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int n=20,sum1,sum2,sum3;
+	sum1=n*(n+1)*0.5;
+	sum2=n*(n+1);
+	sum3=n*n;
+	printf("sum of first five natural number is %d\n",sum1);
+	printf("sum of first five even number is %d\n",sum2);
+	printf("sum of first five odd number is %d\n",sum3);
+	
+}
