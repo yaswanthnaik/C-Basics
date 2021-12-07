f3d28ae9f2cfe3082f92d69836bd6257fe610d14
yaswanthnaik <92287838+yaswanthnaik@users.noreply.github.com>
2021-12-07 10:04:59 +0530

    Add files via upload

---
+++ week.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+	int week;
+	printf("enterr week number between 1-7");
+	scanf("%d",&week);
+	if (week==1)
+	{
+		printf("monday");
+	}
+	else if (week==2)
+	{
+		printf("tuesday");
+	}
+	else if (week==3)
+	{
+		printf("wednesday");
+	}
+	else if (week==4)
+	{
+		printf("thrusday");
+	}
+	else if (week==5)
+	{
+		printf("friday");
+	}
+	else if (week==6)
+	{
+		printf("saturday");
+	}
+	else if (week==7)
+	{
+		printf("sunday");
+	}
+}
+
