b5861ee30ee6e6bb9e49b8f006d325ae4a4394d2
yaswanthnaik <92287838+yaswanthnaik@users.noreply.github.com>
2021-12-08 10:11:06 +0530

    Add files via upload

---
+++ divisibility.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter number");
+	scanf("%d",&a); 
+	if(a%1==0)
+	{
+		printf("%d is divisible by 1\n",a);
+	}
+		if(a%2=<0)
+	{
+		printf("%d is divisible by 2\n",a);
+	}
+		if(a%3==0)
+	{
+		printf("%d is divisible by 3\n",a);
+	}	if(a%4==0)
+	{
+		printf("%d is divisible by 4\n",a);
+	}
+		if(a%5==0)
+	{
+		printf("%d is divisible by 5\n",a);
+	}
+		if(a%6==0)
+	{
+		printf("%d is divisible by 6\n",a);
+	}
+		if(a%7==0)
+	{
+		printf("%d is divisible by 7\n",a);
+	}
+		if(a%8==0)
+	{
+		printf("%d is divisible by 8\n",a);
+	}
+		if(a%9==0)
+	{
+		printf("%d is divisible by 9\n",a);
+	}
+}
