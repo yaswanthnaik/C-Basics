a725b25bbeec8ffda1aa67a0eb19814092db8638
yaswanthnaik <92287838+yaswanthnaik@users.noreply.github.com>
2021-12-08 10:23:05 +0530

    Add files via upload

---
+++ pass,fail.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d,e;
+	printf("enter number");
+	scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+	if(a>35 && b>35 && c>35 && d>35 && e>35 )
+	{
+		printf("pass");
+	}
+	else
+	{
+		printf("fail");
+	}
+}
